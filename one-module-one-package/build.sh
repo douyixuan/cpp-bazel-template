8e675ec65baf27b01d588a3ce464e0307ae3824a
Cedric <14017092+douyixuan@users.noreply.github.com>
2025-02-23 20:02:21 +0800

    feat: compile one module one package

---
+++ one-module-one-package/build.sh
@@ -0,0 +1,8 @@
+bazel build //src:hello-world
+# Should output like this
+#
+# INFO: Found 1 target...
+# Target //src:hello-world up-to-date:
+#   bazel-bin/src/hello-world
+
+./bazel-bin/src/hello-world
